2207df72b91f35757c540f74748b65c6ee5a27de
Tejas Dinkar <tejas@gja.in>
2009-02-03 09:36:13 +0530

    Added the Pie effect to selectsmash

--- selectsmash.cpp
+++ selectsmash.cpp
@@ -2,10 +2,14 @@
 #include "smash.h"
 
 #include "effects/boxing.h"
+#include "effects/pie.h"
 
 #include <QPoint>
 
 Smash *GoferSmash::selectSmash(const QPoint &point)
 {
-	return new BoxingSmash(point, scene);	
+	switch (rand() % 2) {
+		case 0: return new BoxingSmash(point, scene);
+		case 1: return new PieSmash(point, scene);
+	}
 }
